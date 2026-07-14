ec8482beda8f96a6da7570d2ec2e48ba66333fbc
Dev.Prem <prem23375@gmail.com>
2026-07-14 20:02:55 +0530

    Added Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Page from './components/Page'
 import LoginPanel from './components/login/LoginPanel'
+import Home from './components/home/Home'
 
 const App = () => {
   return (
-    <div>
-      {/* <Page/> */}
-      <LoginPanel></LoginPanel>
+    <div className='min-h-screen w-full bg-zinc-900'>
+      <Home></Home>
     </div>
   )
 }
